bcb6931575912f7507e8b39ae9c505d183f93e54
Robbert Wijtman <robbert@xinxmedia.com>
2022-01-01 20:19:27 -0800

    Actually building the graph

--- examples/basicExample/App.js
+++ examples/basicExample/App.js
@@ -1,10 +1,51 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Canvas, Rect } from '@bucky24/react-canvas';
 import { Graph } from '@bucky24/dep-graph';
 
 import styles from './styles.css';
 
+// https://stackoverflow.com/a/7228322
+function randomIntFromInterval(min, max) { // min and max included 
+	return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
 export default function App() {
+	const [nodes, setNodes] = useState([]);
+
+	const rebuildNodes = () => {
+		const colors = ['#f00', '#0f0', '#00f'];
+		const newNodes = [];
+		const nodeCount = randomIntFromInterval(3, 10);
+		for (let i=0;i<nodeCount;i++) {
+			const newNode = {
+				id: `node${i}`,
+				title: `node${i}`,
+				connections: [],
+				color: colors[randomIntFromInterval(0, colors.length-1)],
+			};
+
+			newNodes.push(newNode);
+		}
+
+		const connections = [];
+		const connectCount = randomIntFromInterval(5, 15);
+		for (let i=0;i<connectCount;i++) {
+			const from = randomIntFromInterval(0, newNodes.length-1);
+			const to = randomIntFromInterval(0, newNodes.length-1);
+			const key = `${from}_${to}`;
+			if (from !== to && !connections.includes(key)) {
+				newNodes[from].connections.push(newNodes[to].id);
+				connections.push(key);
+			}
+		}
+
+		setNodes(newNodes);
+	}
+
+	useEffect(() => {
+		rebuildNodes();
+	}, []);
+
 	return (<div className={styles.appRoot}>
 		<Canvas width={600} height={600}>
 			<Rect
@@ -16,7 +57,11 @@ export default function App() {
 				fill={true}
 			/>
 			<Graph
-
+				x={200}
+				y={200}
+				width={200}
+				height={400}
+				nodes={nodes}
 			/>
 			<Rect
 				x={0}
@@ -27,5 +72,6 @@ export default function App() {
 				fill={false}
 			/>
 		</Canvas>
+		<button onClick={rebuildNodes}>Randomize</button>
 	</div>);
 }
--- src/Graph.js
+++ src/Graph.js
@@ -1,11 +1,83 @@
 import React from 'react';
-import { Text } from "@bucky24/react-canvas";
+import { Circle, Line, Text, Rect } from "@bucky24/react-canvas";
 
-export default function Graph() {
+export default function Graph({ nodes, x, y, width, height }) {
+    const useHeight = height-20;
+    const useWidth = width-20;
+    const totalNodes = nodes.length;
+    const anglePerNode = 360/totalNodes;
+    const radAngle = anglePerNode * Math.PI / 180;
+    const builtNodes = nodes.map((node, index) => {
+        const useX = Math.cos(radAngle * index) * useWidth/2;
+        const useY = Math.sin(radAngle * index) * useHeight/2;
+        return {
+            ...node,
+            x: (x+useWidth/2) + useX + 10,
+            y: (y+useHeight/2) + useY + 10,
+        };
+    });
+
+    const nodeById = builtNodes.reduce((obj, node) => {
+        return {
+            ...obj,
+            [node.id]: node,
+        };
+    }, {});
+
     return (
-        <Text
-            x={100}
-            y={100}
-        >Foot</Text>
+        <>
+            <Rect
+                x={x}
+                y={y}
+                x2={x+width}
+                y2={y+height}
+            />
+            {builtNodes.map((node) => {
+                if (!node.connections || node.connections.length === 0) {
+                    return;
+                }
+                return (
+                    <React.Fragment key={node.id}>
+                        {node.connections.map((connection) => {
+                            const nextNode = nodeById[connection];
+
+                            if (!nextNode) {
+                                return null;
+                            }
+
+                            return (
+                                <Line
+                                    key={`${node.id}_${connection}`}
+                                    x={node.x}
+                                    y={node.y}
+                                    x2={nextNode.x}
+                                    y2={nextNode.y}
+                                    color="#f00"
+                                />
+                            )
+                        })}
+                    </React.Fragment>
+                )
+            })}
+            {builtNodes.map((node) => {
+                return (
+                    <React.Fragment key={node.id}>
+                        <Circle
+                            x={node.x}
+                            y={node.y}
+                            radius={10}
+                            color={node.color}
+                            fill={true}
+                        />
+                        <Text
+                            x={node.x-10}
+                            y={node.y+20}
+                        >
+                            {node.title}
+                        </Text>
+                    </React.Fragment>
+                )
+            })}
+        </>
     )
 }
